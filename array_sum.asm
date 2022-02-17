1a1969b594de12a91e56a75f32da70547da29070
Ian <iansulley2@gmail.com>
2022-02-16 20:02:34 -0500

    Corrected array_sum

--- array_sum.asm
+++ array_sum.asm
@@ -1,9 +1,9 @@
 ## Sum an array with a subroutine.
 ## Ian Sulley 02 08 2022
 
-                             # REMOVE this comment and the next one
-PR_INT = 1                   # (example of defining a constant)
         .data
+str:	.asciiz "Sum of all Array elements\n"
+
 array:  .word -123, 548, 923, 431, 560, -348, 961
 endarr: 
         .text
@@ -15,25 +15,34 @@ main:   la $a0, array        # a0 points into array
 	addi $sp, $sp, -4    # push 
 	sw $ra, 0($sp)       #   ret addr on stack
         jal arrsum
+        add $s0, $0, $v0     # copy sum into $s0
 	lw $ra, 0($sp)       # pop
 	addi $sp, $sp, 4     #   ret addr
-        add $a0, $0, $v0     # copy sum into $a0
-        addi $v0, $0, PR_INT # print code in $v0
-        syscall
-        jr $ra                  
-
+	# print out the result string
+	li $v0, 4			# system call code for printing string = 4
+	la $a0, str		# load address of string to be printed into $a0
+	syscall				# call operating system to perform print operation
+	# print out integer value in $t1
+	add $a0, $0, $s0 		# move integer to be printed into $a0:  $a0 = $s0
+	li $v0, 1 
+	syscall				# call operating system to perform print
+	# exit program
+	li $v0, 10			# system call code for exit = 10
+	syscall	
+	jr $ra
 ## array-summation subroutine
 ## register use:
 ##	$a0: parameter: array addr; used as pointer to current element
 ##	$a1: parameter: size of arr 
 ##	$v0: accumulator and return value
 ##	$t2: temporary copy of current array element
-arrsum: move $v0, $0         # v0 accumulates sum
-        move $t3, $0
+arrsum: li $v0, 0         # v0 accumulates sum
+        li $t3, 0
         j test               #    and do it again
 sum:    lw $t2, 0($a0)       #    get next array element
         add $v0, $v0, $t2    #    add it in
-        sll $a0, $a0, 2      #    point to next word
+        addi $a0, $a0, 4      #    point to next word
         addi $t3, $t3, 1     # count++
-test:   ble $t3, $a1, done   # while t3 < len(arr) do
-done:   jr $ra
+test:   blt $t3, $a1, sum    # while t3 < len(arr) do
+	jr $ra
+
